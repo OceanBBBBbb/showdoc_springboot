853d22f5cc78ac05721ba0daf24b5c549976a99c
oceanbbbbbb <bhy71090@163.com>
2020-03-30 15:08:09 +0800

    init

---
+++ src/main/java/com/ocean/showdoc/ShowdocApplication.java
@@ -0,0 +1,13 @@
+package com.ocean.showdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowdocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowdocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ocean/showdoc/ShowdocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocean.showdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowdocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
